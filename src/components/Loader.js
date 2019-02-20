02a8c4c69781b8bbd8978bf2184cdf26d7b691ca
Zaur Bairamau <bairamau.z@gmail.com>
2019-02-21 02:38:09 +0300

    add Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { PulseLoader } from "react-spinners";
+
+const Loader = () => (
+  <div className="loader-container">
+    <PulseLoader />
+  </div>
+);
+
+export default Loader;
