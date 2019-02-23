7680697b4c72a56a6584a8b842b306dbff707307
Zaur Bairamau <bairamau.z@gmail.com>
2019-02-23 22:50:13 +0300

    add Arrivals api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ const APP_ID = "daf0bf3c";
 const APP_KEY = "ef5eaecdae3d38ef0fb1d0e0ffd2c216";
 const authQuery = `app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-export async function fetchData(resource) {
+async function fetchData(resource) {
   const response = await fetch(
     `https://api.tfl.gov.uk${resource}?${authQuery}`
   );
@@ -14,25 +14,10 @@ export async function getStations() {
   return stopPoints.filter(item => item.stopType === "NaptanMetroStation");
 }
 
-// https://api.tfl.gov.uk/bikepoint
-// List all Santander Cycle hire docking stations, including 5 minute real-time bike and docking station status information.
+export async function getAllArrivals(naptanId) {
+  return await fetchData(`/StopPoint/${naptanId}/Arrivals`);
+}
 
-// /Line/Meta/Modes
-// bus
-// cable-car
-// coach
-// cycle
-// cycle-hire
-// dlr
-// interchange-keep-sitting
-// interchange-secure
-// national-rail
-// overground
-// replacement-bus
-// river-bus
-// river-tour
-// taxi
-// tflrail
-// tram
-// tube
-// walking
+export async function getArrivalsByLine(lineId, naptanId) {
+  return await fetchData(`/Line/${lineId}/Arrivals/${naptanId}`);
+}
