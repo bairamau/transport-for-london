94c6db6946490073111251727c66e467982b6ac1
Zaur Bairamau <bairamau.z@gmail.com>
2019-02-24 06:13:07 +0300

    add Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Menu, Icon } from "antd";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <Menu mode="horizontal" theme="dark">
+        <Menu.Item key="search">
+          <Link to='/search'>
+            <Icon type="search"/>
+            Search
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="favourited">
+          <Link to='/favourted'>
+            <Icon type="star" />
+            Favourited
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default Nav;
